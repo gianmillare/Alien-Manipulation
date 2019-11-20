e0d890040c06e034b0fff18e85194d13cc781745
Gian Millare <gianmillare@Gians-MacBook-Pro.local>
2019-11-19 18:10:37 -0800

    completion of js

--- Aliens/alien-manipulation.js
+++ Aliens/alien-manipulation.js
@@ -5,8 +5,9 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // DISPLAYING THE DATASET ON THE HTML APPLICATION
-function displayData(){
-	data.forEach(function(sightings) {
+function displayData(variable){
+	tbody.html("");
+	variable.forEach(function(sightings) {
 	
 		var row = tbody.append("tr");
 		Object.entries(sightings).forEach(function([key, value]) {
@@ -16,22 +17,24 @@ function displayData(){
 		});
 	});
 };
-displayData(data);
 
-// SELECT THE BUTTON AND USER INPUT
 var button = d3.select("#filter-btn");
-var inputValue = d3.select("#datetime").property("value");
 
 function buttonClick() {
+	// SELECT THE BUTTON AND USER INPUT
+	var inputValue = d3.select("#datetime").property("value");
+	console.log(inputValue)
 	// PREVENT PAGE REFRESH
 	d3.event.preventDefault();
 	// DISPLAY THE INPUT
 	console.log(inputValue);
 	// FILTER OUT THE DATA
 	var filteredData = tableData.filter(alien => alien.datetime === inputValue);
+	console.log(filteredData);
 	// DISPLAY THE NEW DATASET
 	displayData(filteredData);
 };
-inputValue.on("change", buttonClick);
+button.on("click", buttonClick);
 
+displayData(tableData);
 
